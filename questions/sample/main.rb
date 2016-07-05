a8ba253d95805e6c0a86ba612a466d6ed7523f80
nana <keyaki.436vrwgz@gmail.com>
2016-07-05 20:16:21 +0900

    写経中

--- questions/sample/main.rb
+++ questions/sample/main.rb
@@ -5,7 +5,7 @@ module Q00
 
   @memo = {}
 
-  def run
+  def run_back(n)
     return @memo[n] if @memo.has_key?(n)
     if (n == 0) || (n == 1) then
       @memo[n] = 1
@@ -13,11 +13,32 @@ module Q00
       @memo[n] = run(n - 1) + run(n - 2)
     end
   end
+
+  # フィボナッチ数列のうち、各桁の数字を足した数で割り切れる数を、小さい方から5個求める
+  def run(n)
+    # 1つ目と2つ目っていう意味での変数なのかな。
+    a = b = 1
+    count = 0
+    # なぜ11？？
+    while (count < 11) do
+      c = a + b
+      sum = 0
+      # ここで//でsplitしているのはなぜ？
+      c.to_s.split(//).each{|e| sum += e.to_i}
+      if (c % sum == 0) then
+      #   割り切れた場合に出力する
+        puts c
+        count += 1
+      end
+      # なぜbとcをaとbに入れているの？？
+      a, b = b, c
+    end
+  end
 end
 
 Benchmark.bm do |x|
   x.report do
-    $answer = Q00.run
+    $answer = Q00.run(10)
   end
 end
 
